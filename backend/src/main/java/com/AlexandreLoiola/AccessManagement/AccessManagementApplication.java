eae76f39ca69ce7d9997242e31ccb9a273413966
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2024-02-06 22:14:18 -0300

    chore: Initial Backend Setup

---
+++ backend/src/main/java/com/AlexandreLoiola/AccessManagement/AccessManagementApplication.java
@@ -0,0 +1,12 @@
+package com.AlexandreLoiola.AccessManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessManagementApplication.class, args);
+	}
+}
