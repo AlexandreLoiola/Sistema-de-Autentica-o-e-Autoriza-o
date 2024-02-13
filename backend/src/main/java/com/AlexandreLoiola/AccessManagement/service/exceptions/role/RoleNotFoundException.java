df12c22b56d95531db391e5f74f7499e4f8ee492
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2024-02-12 23:09:24 -0300

    feat: Add role exceptions
    - RoleInsertException
    - RoleNotFoundException
    - RoleUpdateException

---
+++ backend/src/main/java/com/AlexandreLoiola/AccessManagement/service/exceptions/role/RoleNotFoundException.java
@@ -0,0 +1,9 @@
+package com.AlexandreLoiola.AccessManagement.service.exceptions.role;
+
+public class RoleNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public RoleNotFoundException(String msg) { super(msg); }
+
+    public RoleNotFoundException(String msg, Throwable cause) {super(msg, cause);}
+}
