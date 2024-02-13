deb0f2bc88a1f74f18631ce91b7355fb87f33d40
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2024-02-13 14:51:26 -0300

    feat: Add new pages and rename globalstyles

---
+++ frontend/src/Pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p style={{"color": "red"}}>aaaaaaaaaaaaaaaaaa</p>
+    </div>
+  );
+};
+export default Home;
---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import Home from "../Pages/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+export default router;
