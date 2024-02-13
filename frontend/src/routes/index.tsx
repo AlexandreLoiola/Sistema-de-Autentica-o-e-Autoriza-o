09c1dfca9b5c9f1d3efaab815d4b5ac4737068a4
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2024-02-13 20:57:17 -0300

    feat: create recoverpass page

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -4,6 +4,7 @@ import App from "../App";
 import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
+import RecoverPass from "../Pages/RecoverPass/RecoverPass";
 
 const router = createBrowserRouter([
   {
@@ -18,6 +19,10 @@ const router = createBrowserRouter([
       {
         path: "/login",
         element: <Login />,
+      },
+      {
+        path: "/forgot-password",
+        element: <RecoverPass />,
       },
     ],
   },
