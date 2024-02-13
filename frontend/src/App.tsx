5accdbceaaa4447f62014e87417b08f4bd8cdadf
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2024-02-13 11:30:52 -0300

    feat: Add React Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,13 @@
-function App() {
-  return <>Sistema de Autenticação e Autorização</>;
-}
+import React from "react";
+import { GlobalStyle } from "./Styles/GlobalStyles";
+import { Outlet } from "react-router-dom";
 
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <Outlet />
+    </div>
+  );
+};
 export default App;
