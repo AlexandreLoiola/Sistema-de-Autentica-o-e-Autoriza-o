604fe6051c9c4b9cebf3a95cc0a58c11bc4505cb
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2024-04-17 18:04:19 -0300

    feat: Add User Not Found Exception

--- backend/src/main/java/com/AlexandreLoiola/AccessManagement/service/exceptions/user/UserNotFoundException.java
+++ backend/src/main/java/com/AlexandreLoiola/AccessManagement/service/exceptions/user/UserNotFoundException.java
@@ -1,8 +1,9 @@
 package com.AlexandreLoiola.AccessManagement.service.exceptions.user;
 
-import jakarta.persistence.EntityNotFoundException;
 
-public class UserNotFoundException extends EntityNotFoundException {
+import org.springframework.security.core.AuthenticationException;
+
+public class UserNotFoundException  extends AuthenticationException {
     private static final long serialVersionUID = 1L;
 
     public UserNotFoundException(String msg) { super(msg); }
