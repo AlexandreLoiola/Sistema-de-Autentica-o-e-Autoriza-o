d0a7d4aefb464ca1ea568a49bd2919f7406330d1
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2024-04-17 11:38:33 -0300

    feat: Test Main Class

--- backend/src/test/java/com/AlexandreLoiola/Access/Management/AccessManagementApplicationTests.java
+++ backend/src/test/java/com/AlexandreLoiola/Access/Management/AccessManagementApplicationTests.java
@@ -1,13 +1,22 @@
 package com.AlexandreLoiola.Access.Management;
 
+import com.AlexandreLoiola.AccessManagement.AccessManagementApplication;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest
+@SpringBootTest(classes = AccessManagementApplication.class)
 class AccessManagementApplicationTests {
+
+	@MockBean
+	private SecurityAutoConfiguration securityAutoConfiguration;
 
 	@Test
 	void contextLoads() {
+		String[] args = {};
+		AccessManagementApplication.main(args);
 	}
 
 }
