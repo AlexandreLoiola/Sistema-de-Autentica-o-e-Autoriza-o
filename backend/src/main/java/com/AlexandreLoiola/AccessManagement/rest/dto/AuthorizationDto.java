b6ed188456e90e8dad9858b4e8bc913b5532e91f
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2024-06-24 18:24:29 -0300

    feat: Create hash and equals in AuthorizationDto

--- backend/src/main/java/com/AlexandreLoiola/AccessManagement/rest/dto/AuthorizationDto.java
+++ backend/src/main/java/com/AlexandreLoiola/AccessManagement/rest/dto/AuthorizationDto.java
@@ -2,6 +2,7 @@ package com.AlexandreLoiola.AccessManagement.rest.dto;
 
 import lombok.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 @Getter @Setter
@@ -11,4 +12,19 @@ public class AuthorizationDto {
     private String description;
     private String path;
     private Set<MethodDto> methods;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AuthorizationDto that = (AuthorizationDto) o;
+        return Objects.equals(description, that.description) &&
+                Objects.equals(path, that.path) &&
+                Objects.equals(methods, that.methods);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(description, path, methods);
+    }
 }
