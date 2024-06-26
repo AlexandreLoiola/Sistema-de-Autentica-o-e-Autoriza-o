6bd5cf630d5d50adbb65ef4f04e408670e86810f
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2024-06-25 21:33:59 -0300

    fix: Create hash and equals methods on RoleDto

--- backend/src/main/java/com/AlexandreLoiola/AccessManagement/rest/dto/RoleDto.java
+++ backend/src/main/java/com/AlexandreLoiola/AccessManagement/rest/dto/RoleDto.java
@@ -2,6 +2,7 @@ package com.AlexandreLoiola.AccessManagement.rest.dto;
 
 import lombok.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 @Getter @Setter
@@ -10,4 +11,18 @@ import java.util.Set;
 public class RoleDto {
     private String description;
     private Set<AuthorizationDto> authorizations;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RoleDto that = (RoleDto) o;
+        return Objects.equals(description, that.description) &&
+                Objects.equals(authorizations, that.authorizations);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(description, authorizations);
+    }
 }
