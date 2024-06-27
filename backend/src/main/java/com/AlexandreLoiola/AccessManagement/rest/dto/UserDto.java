c13cd44404885e02e5307fe52c5da9cf0deb79e9
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2024-06-26 21:09:01 -0300

    fix: Create hash and equals methods on UserDto

--- backend/src/main/java/com/AlexandreLoiola/AccessManagement/rest/dto/UserDto.java
+++ backend/src/main/java/com/AlexandreLoiola/AccessManagement/rest/dto/UserDto.java
@@ -2,6 +2,7 @@ package com.AlexandreLoiola.AccessManagement.rest.dto;
 
 import lombok.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 @Getter @Setter
@@ -11,4 +12,19 @@ public class UserDto {
     private String email;
     private String username;
     private Set<RoleDto> roles;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserDto userDto = (UserDto) o;
+        return Objects.equals(email, userDto.email) &&
+                Objects.equals(username, userDto.username) &&
+                Objects.equals(roles, userDto.roles);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, username, roles);
+    }
 }
